23c589609cf02264b6d7700352a36b33eed46742
aaronbauer <aaron.bauer@freenet.de>
2011-09-03 17:51:21 +0200

    Wasser 0.8
    Temperature of the day before is now visible by swiping to the right,
    including the description and the coloured numbers. Small bugfixes.

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ Datum: 29.08.2011
 Setzt voraus, dass das daemon.php stündlich ausgeführt wird.
 */
 
-$version = '0.7.1';
-$build = '64eac5';
+$version = '0.8';
+$build = 'xxxxxx';
 
 $versioning = 'Version: '.$version.' ('.$build.')'; 
 
 // Hier den Ort eintragen
-$directory = 'http://wasser.aaronbauer.org';
+$directory = 'http://wasser.aaronbauer.org/';
 
 // $date wird für das Datum im XML verwendet
 $date = date('D, d M Y H:i:s T');
@@ -31,11 +31,20 @@ $db_selected = mysql_select_db('d011c151', $link);
 if(!$db_selected) {
     die ('Kann Datenbank nicht nutzen: ' .mysql_error());
 } else {
-       
+     
+    // Holt die aktuelle Wassertemperatur  
     $query = 'SELECT * FROM wasser ORDER BY id DESC';
     $result = mysql_query($query) or die(mysql_error());
     
     $data = mysql_fetch_array($result) or die(mysql_error());
+    
+    // Holt die Temperatur vom Vortag
+    $previous_query = 'SELECT * FROM wasser
+WHERE site_date <> "'.$data['site_date'].'" ORDER BY id DESC';
+
+    $previous_data_result = mysql_query($previous_query) or die(mysql_error());
+    
+    $previous_data = mysql_fetch_array($previous_data_result) or die(mysql_error());
     
     mysql_close($link);
 };
@@ -50,7 +59,7 @@ SELECT * FROM 'wasser'
 Die Bezeichnungen von 18-26. Wenn $_GET auf "en" steht wird die englische Version ausgegeben. Ansonsten die normale.
 $lang_link ist einfach nur der passende Link für die Website (damit das HTML sauber bleibt). 
 */
-if($data['temperature'] == '00') {
+if($data['temperature'] == '--') {
     echo 'Keine Daten.';
 } else {
     switch ($data['temperature']) {
@@ -92,7 +101,46 @@ if($data['temperature'] == '00') {
         break;        
 };
 };
- 
+
+// Beschreibung für die Temperatur vom Vortag
+switch ($previous_data['temperature']) {
+    case 26:
+        $previous_description = 'Viel zu warm!';
+        $previous_color = '#ff0033';
+        break;
+    case 25:
+        $previous_description = 'Sehr warm!';
+        $previous_color = '#ff3000';
+        break;
+    case 24:
+        $previous_description = 'Warm!';
+        $previous_color = '#ff3000';
+        break;
+    case 23:
+        $previous_description = 'Warm genug';
+        $previous_color = '#ff5202';
+        break;
+    case 22:
+        $previous_description = 'Angenehm';
+        $previous_color = '#ffa600';
+        break;
+    case 21:
+        $previous_description = 'Noch okay';
+        $previous_color = '#ffdd00';
+        break;
+    case 20:
+        $previous_description = 'Etwas k&uuml;hl';
+        $previous_color = '#dfff00';
+        break;
+    case 19:
+        $previous_description = 'Kalt';
+        $previous_color = '#00c3ff';
+        break;
+    case 18:
+        $previous_description = 'Zu Kalt';
+        $previous_color = '#00c3ff';
+        break;  
+}; 
 
 
 ?>
@@ -125,6 +173,57 @@ and (max-device-width: 460px)" href="<?php echo $directory; ?>/iphone.css" type=
 <!-- Computer Stylesheet -->
 <link rel="stylesheet" media="only screen and (min-device-width: 1025px)" href="<?php echo $directory; ?>/standard.css" type="text/css" />
 
+<!-- jQuery -->
+<script type="text/javascript" src="<?php echo $directory; ?>/jquery-1.6.2.js"></script>
+
+<!-- Tolles Touch script von hier: http://james.lin.net.nz/2010/09/27/javascript-swipe-demo-works-both-with-finger-ipad-and-mouse-pc/ -->
+<script type="text/javascript">// <![CDATA[
+var $j = jQuery.noConflict();
+var down_x = null;
+var up_x = null;
+$j().ready(function(){
+  $j("#slider > div").mousedown(function(e){
+    e.preventDefault();
+    down_x = e.pageX;
+  });
+  $j("#slider > div").mouseup(function(e){
+    up_x = e.pageX;
+    do_work();
+  });
+  $j("#slider > div").bind('touchstart', function(e){
+    down_x = e.originalEvent.touches[0].pageX;
+  });
+  $j("#slider > div").bind('touchmove', function(e){
+    e.preventDefault();
+    up_x = e.originalEvent.touches[0].pageX;
+  });
+  $j("#slider > div").bind('touchend', function(e){
+    do_work();
+  });
+});
+function do_work()
+{
+  if ((down_x - up_x) > 50)
+    {
+        slide_right();
+    }
+    if ((up_x - down_x) > 50)
+    {
+        slide_left();
+    }
+}
+function slide_right()
+{
+  $j("#slider_content").animate({scrollLeft:'+=320'},1000);
+}
+function slide_left()
+{
+  $j("#slider_content").animate({scrollLeft:'-=320'},1000);
+}
+// ]]></script>
+
+
+
 <style type="text/css">
 h1 {
     font-size: 65pt;
@@ -155,13 +254,27 @@ p{
 </head>
 <body>
 <div id="wrap">
-<div id="layer">
-<h1><?php echo $data['temperature']; ?>&deg;C</h1>
-<h2><?php echo $description; ?></h2>
-<p>Gemessen am <b><?php echo $data['site_date']; ?></b> <br /> um <b><?php echo $data['site_time']; ?></b>.</p>
-<p class="version"><?php echo $versioning; ?></p>
+    <div id="slider_content" style="width: 320px; height: 460px; overflow: hidden;">
+        <div id="slider" style="height: 460px; width: 640px;">
+        <!-- Temperatur von heute -->
+            <div id="slide1" style="height: 460px; width: 320px; float:left;">
+                <div id="layer">
+                    <h1><?php echo $data['temperature']; ?>&deg;C</h1>
+                    <h2><?php echo $description; ?></h2>
+                    <p>Gemessen am <b><?php echo $data['site_date']; ?></b> <br /> um <b><?php echo $data['site_time']; ?></b>.</p>
+                </div>
+            </div>
+        <!-- Temperatur von gestern -->
+            <div id="slide2" style="height: 460px; width: 320px; float: left;">
+                <div id="layer">
+                    <h1 style="color:<?php echo $previous_color; ?>;"><?php echo $previous_data['temperature']; ?>&deg;C</h1>
+                    <h2><?php echo $previous_description; ?></h2>
+                    <p>Gemessen am <b><?php echo $previous_data['site_date']; ?></b> <br /> um <b><?php echo $previous_data['site_time']; ?></b>.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Daemon war zuletzt da: <?php echo $data['cur_timestamp']; ?> -->
-</div>
-</div>
 </body>
 </html>
